be75327ca72ea37d9d92f3a27bd3b78b22e8254e
Chris Alley <chris@chrisalley.info>
2023-09-23 00:03:10 +1200

    Add Prettier to the project

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,83 +1,83 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect } from 'vitest'
 import {
   camelise,
   dasherise,
   downcaseFirst,
   parameterise,
   titleise,
-  underscore,
-} from "./index";
+  underscore
+} from './index'
 
-describe("camelise", () => {
-  test("converts underscored string to camelcase with uppercase first letter by default", () => {
-    expect(camelise("first_second_third")).toEqual("FirstSecondThird");
-  });
+describe('camelise', () => {
+  test('converts underscored string to camelcase with uppercase first letter by default', () => {
+    expect(camelise('first_second_third')).toEqual('FirstSecondThird')
+  })
 
-  test("converts underscored string to camelcase with lowercase first letter when uppercase first letter is false", () => {
-    expect(camelise("first_second_third", false)).toEqual("firstSecondThird");
-  });
-});
+  test('converts underscored string to camelcase with lowercase first letter when uppercase first letter is false', () => {
+    expect(camelise('first_second_third', false)).toEqual('firstSecondThird')
+  })
+})
 
-describe("dasherise", () => {
-  test("converts underscored string to a hyphenated string", () => {
-    expect(dasherise("first_second_third")).toEqual("first-second-third");
-  });
-});
+describe('dasherise', () => {
+  test('converts underscored string to a hyphenated string', () => {
+    expect(dasherise('first_second_third')).toEqual('first-second-third')
+  })
+})
 
-describe("downcaseFirst", () => {
-  test("makes the first character of the first word lowercase", () => {
-    expect(downcaseFirst("First Second Third")).toEqual("first Second Third");
-  });
-});
+describe('downcaseFirst', () => {
+  test('makes the first character of the first word lowercase', () => {
+    expect(downcaseFirst('First Second Third')).toEqual('first Second Third')
+  })
+})
 
-describe("parameterise", () => {
-  test("replaces spaces with hyphens and converts to lowercase by default", () => {
-    expect(parameterise("First Second Third")).toEqual("first-second-third");
-  });
+describe('parameterise', () => {
+  test('replaces spaces with hyphens and converts to lowercase by default', () => {
+    expect(parameterise('First Second Third')).toEqual('first-second-third')
+  })
 
-  test("replaces spaces with a custom seperator if specified", () => {
-    expect(parameterise("First Second Third", "_")).toEqual(
-      "first_second_third"
-    );
-  });
+  test('replaces spaces with a custom seperator if specified', () => {
+    expect(parameterise('First Second Third', '_')).toEqual(
+      'first_second_third'
+    )
+  })
 
-  test("preserves the case of characters in a string if specified", () => {
-    expect(parameterise("First Second Third", "-", true)).toEqual(
-      "First-Second-Third"
-    );
-  });
+  test('preserves the case of characters in a string if specified', () => {
+    expect(parameterise('First Second Third', '-', true)).toEqual(
+      'First-Second-Third'
+    )
+  })
 
-  test("preserves dashes and underscores unless they are used as separators", () => {
-    expect(parameterise("First Second__")).toEqual("first-second__");
-    expect(parameterise("First Second--", "_")).toEqual("first_second--");
-    expect(parameterise("First_Second--", ".")).toEqual("first_second--");
-  });
-});
+  test('preserves dashes and underscores unless they are used as separators', () => {
+    expect(parameterise('First Second__')).toEqual('first-second__')
+    expect(parameterise('First Second--', '_')).toEqual('first_second--')
+    expect(parameterise('First_Second--', '.')).toEqual('first_second--')
+  })
+})
 
-describe("titleise", () => {
-  test("capitalises each space seperated word in the string", () => {
-    expect(titleise("first second third")).toEqual("First Second Third");
-  });
+describe('titleise', () => {
+  test('capitalises each space seperated word in the string', () => {
+    expect(titleise('first second third')).toEqual('First Second Third')
+  })
 
-  test("capitalises each underscore seperated word, replacing underscores with spaces", () => {
-    expect(titleise("first_second_third")).toEqual("First Second Third");
-  });
+  test('capitalises each underscore seperated word, replacing underscores with spaces', () => {
+    expect(titleise('first_second_third')).toEqual('First Second Third')
+  })
 
-  test("adds a space between each word that already starts with a capital", () => {
-    expect(titleise("FirstSecondThird")).toEqual("First Second Third");
-  });
+  test('adds a space between each word that already starts with a capital', () => {
+    expect(titleise('FirstSecondThird')).toEqual('First Second Third')
+  })
 
-  test("remove additional spaces in around and within title", () => {
-    expect(titleise("  first  second  third  ")).toEqual("First Second Third");
-  });
-});
+  test('remove additional spaces in around and within title', () => {
+    expect(titleise('  first  second  third  ')).toEqual('First Second Third')
+  })
+})
 
-describe("underscore", () => {
-  test("converts camelcase string to underscore seperated string", () => {
-    expect(underscore("firstSecondThird")).toEqual("first_second_third");
-  });
+describe('underscore', () => {
+  test('converts camelcase string to underscore seperated string', () => {
+    expect(underscore('firstSecondThird')).toEqual('first_second_third')
+  })
 
-  test("converts pascalcase string to underscore seperated string", () => {
-    expect(underscore("FirstSecondThird")).toEqual("first_second_third");
-  });
-});
+  test('converts pascalcase string to underscore seperated string', () => {
+    expect(underscore('FirstSecondThird')).toEqual('first_second_third')
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -3,68 +3,68 @@ export function camelise(
   uppercaseFirstLetter: boolean = true
 ): string {
   return words
-    .split("_")
+    .split('_')
     .map((word, index) => {
       if (index === 0 && !uppercaseFirstLetter) {
-        return word;
+        return word
       }
-      return word.charAt(0).toUpperCase() + word.slice(1);
+      return word.charAt(0).toUpperCase() + word.slice(1)
     })
-    .join("");
+    .join('')
 }
 
 export function dasherise(words: string): string {
-  return words.replaceAll("_", "-");
+  return words.replaceAll('_', '-')
 }
 
 export function downcaseFirst(words: string): string {
   return words
-    .split(" ")
+    .split(' ')
     .map((word, index) => {
-      return index === 0 ? word.charAt(0).toLowerCase() + word.slice(1) : word;
+      return index === 0 ? word.charAt(0).toLowerCase() + word.slice(1) : word
     })
-    .join(" ");
+    .join(' ')
 }
 
 export function parameterise(
   word: string,
-  seperator = "-",
+  seperator = '-',
   preserveCase = false
 ): string {
-  const stringToParameterise = preserveCase ? word : word.toLowerCase();
-  return stringToParameterise.split(" ").join(seperator);
+  const stringToParameterise = preserveCase ? word : word.toLowerCase()
+  return stringToParameterise.split(' ').join(seperator)
 }
 
 export function titleise(words: string): string {
   return words
     .trim()
-    .replaceAll("_", " ")
-    .split("")
+    .replaceAll('_', ' ')
+    .split('')
     .map((letter, index) => {
       if (index !== 0 && letterIsUpperCase(letter)) {
-        return ` ${letter}`;
+        return ` ${letter}`
       }
-      return letter;
+      return letter
     })
-    .join("")
-    .split(" ")
+    .join('')
+    .split(' ')
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .filter((word) => word !== "")
-    .join(" ");
+    .filter((word) => word !== '')
+    .join(' ')
 }
 
 export function underscore(words: string): string {
   return words
-    .split("")
+    .split('')
     .map((letter, index) => {
       if (index !== 0 && letterIsUpperCase(letter)) {
-        return `_${letter.toLowerCase()}`;
+        return `_${letter.toLowerCase()}`
       }
-      return letter.toLowerCase();
+      return letter.toLowerCase()
     })
-    .join("");
+    .join('')
 }
 
 function letterIsUpperCase(letter: string): boolean {
-  return letter.toUpperCase() === letter && letter.toLowerCase() !== letter;
+  return letter.toUpperCase() === letter && letter.toLowerCase() !== letter
 }
