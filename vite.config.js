e299d97ba219352453592773d9e6d83f424f5e06
Chris Alley <chris@chrisalley.info>
2026-04-03 13:22:19 +1300

    build: include types in the distribution

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'unplugin-dts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,10 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'string-instrument'
+      name: 'string-instrument',
+      formats: ['es'],
+      fileName: 'string-instrument'
     }
-  }
+  },
+  plugins: [dts({ tsconfigPath: './tsconfig.build.json', bundleTypes: true })]
 })
